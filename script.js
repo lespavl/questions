6805391625fd6e5c0dc2f411470ecf25b277b410
olesia.korotkova <lespavl@gmail.com>
2024-10-09 22:37:46 +0200

    fix for getting 10000+ items

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const getRandomQuestions = (questions, count) => {
 };
 
 const getSequentialQuestions = (questions, start, count) => {
-    if(start > 10000) {
-        start = 2805 + (start - 10000);
-    }
     return questions.slice(start - 1, start - 1 + count);
 };
 
