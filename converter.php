64b405670fdc36c785e4bcebaca1688f9ecb0f18
olesia.korotkova <lespavl@gmail.com>
2024-10-09 22:23:49 +0200

    more questions

--- converter.php
+++ converter.php
@@ -1,52 +1,83 @@
 <?php
-// Чтение JSON файла
-$inputJson = file_get_contents('original.json');
-$data = json_decode($inputJson, true);
 
-$questions = [];
-$currentQuestion = [
-    'number' => '',
-    'text' => '',
-    'answers' => [],
-    'correct' => ''
-];
-$errors = 0;
+function loadJsonFile($filename) {
+    $json = file_get_contents($filename);
+    return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+}
 
-foreach ($data as $item) {
-    if (isset($item['Column1']) || isset($item['Column2'])) {
-        if (!empty($currentQuestion['number']) && !empty($currentQuestion['text'])) {
-            if(count($currentQuestion['answers']) != 4) {
-                $errors++;
-            }
-            $questions[] = $currentQuestion;
-            $currentQuestion = [
-                'number' => '',
-                'text' => '',
-                'answers' => [],
-                'correct' => ''
-            ];
+function convert(string $original, string $output): void
+{
+    $data = loadJsonFile($original);
+
+    $questions = [];
+    $currentQuestion = [
+        'number' => '',
+        'text' => '',
+        'answers' => [],
+        'correct' => ''
+    ];
+    $errors = 0;
+
+    foreach ($data as $item) {
+        if(empty($item)) {
+            continue;
         }
 
-            $currentQuestion['number'] = isset($item['Column1']) ? $item['Column1'] : $currentQuestion['number'];
-            $currentQuestion['text'] = isset($item['Column2']) ? $item['Column2'] : $currentQuestion['text'];
+        if (isset($item['Column1']) || isset($item['Column2'])) {
+
+            if(empty($item['Column3'])) {
+                continue;
+            }
+
+            if (!empty($currentQuestion['number']) && !empty($currentQuestion['text'])) {
+                if (count($currentQuestion['answers']) != 4) {
+                    $errors++;
+                }
+                $questions[] = $currentQuestion;
+                $currentQuestion = [
+                    'number' => '',
+                    'text' => '',
+                    'answers' => [],
+                    'correct' => ''
+                ];
+            }
+
+            $currentQuestion['number'] = $item['Column1'] ?? $currentQuestion['number'];
+            $currentQuestion['text'] = $item['Column2'] ?? $currentQuestion['text'];
+        }
+
+        $currentQuestion['answers'][] = $item['Column3'];
+        if ($item['Column4'] == 'правильна') {
+            $currentQuestion['correct'] = $item['Column3'];
+        }
     }
 
-
-    $currentQuestion['answers'][] = $item['Column3'];
-    if ($item['Column4'] == 'правильна') {
-        $currentQuestion['correct'] = $item['Column3'];
+    if ($currentQuestion) {
+        $questions[] = $currentQuestion;
     }
+
+    $outputJson = json_encode($questions, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    file_put_contents($output, $outputJson);
+    echo "Преобразование завершено.\n";
+    echo "Ошибок: {$errors}\n";
 }
 
-if ($currentQuestion) {
-    $questions[] = $currentQuestion;
+function combine($file1, $file2, $output)
+{
+    $file1 = loadJsonFile($file1);
+    $file2 = loadJsonFile($file2);
+
+    foreach ($file2 as &$item) {
+        $item['number'] = 10000 + $item['number'];
+    }
+
+    $combined = array_merge($file1, $file2);
+    file_put_contents($output, json_encode($combined, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    echo "Файлы успешно объединены!";
 }
 
-$outputJson = json_encode($questions, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
-// Запись JSON в файл
-file_put_contents('questions.json', $outputJson);
+convert('original1.json', 'questions1.json');
+convert('original2.json', 'questions2.json');
+combine('questions1.json', 'questions2.json', 'questions.json');
 
-echo "Преобразование завершено.\n";
-echo "Ощибок: .{$errors}\n";
-?>
